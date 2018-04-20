45f8b2d565d1851861a1de852df68444453cdac9
msweeneydev <matthewsweeney@mailup.net>
2018-04-20 12:59:30 +0100

    setup files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import glamorous, {ThemeProvider} from 'glamorous';
+
+const { P } = glamorous;
+
+const Header = () => (
+  <div>
+    <P color="red">Hello, HEAD.</P>
+    <p>Header, again</p>
+  </div>
+)
+
+export default Header
